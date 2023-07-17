bbf30789a68eeeefbe8323311ec1ea69dc25065d
ismael-haque <ismaelhaque.dev@gmail.com>
2023-07-17 10:39:23 +0600

    create and use DB

---
+++ createAndUseDB.sql
@@ -0,0 +1,16 @@
+-- show a list of existing databases
+SHOW databases;
+
+-- Use a perticular database
+USE gregs_list;
+
+-- Show tables in a database
+SHOW tables;
+
+-- Describe tables
+--  'describe table_name;' shows all the fields in a table
+DESC doughnut_list; 
+DESCRIBE my_contacts;
+DESCRIBE gregs_list.doughnut_list;
+DESCRIBE gregs_list.my_contacts;
+ 
